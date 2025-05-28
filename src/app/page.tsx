1afb294375038f111fdc9e0397f23adecd3078a2
bakuyy <sophieyang12345@gmail.com>
2025-05-28 14:20:05 -0400

    feat: add auth page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import LoginPage from "./login/page";
+"use client";
+import AuthForm from "@/components/AuthForm";
+import Image from "next/image";
+import Footer from "../../public/images/login/footer.png"
+import Header from "../../public/images/login/header.png"
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center gap-4">
-      <div className="flex flex-col items-center gap-4">
-        <LoginPage/>
-      </div>
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <Image
+      src={Header} alt="Header" 
+      className="w-full h-1/5 absolute top-0 opacity-70"/>
+      <AuthForm/>
+      <Image
+      src={Footer} alt="Footer" 
+      className="w-full h-1/5 absolute bottom-0 opacity-70"/>
     </main>
   );
 }

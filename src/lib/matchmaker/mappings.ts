649eb593666a6ba10b7e8557103dc01f03991889
iydia <hly.liya@gmail.com>
2025-05-28 17:26:51 -0400

    Multiple choice question mapping

---
+++ src/lib/matchmaker/mappings.ts
@@ -0,0 +1,15 @@
+export const classBehaviorMap: Record<string, string> = {
+  a: "Sit in the front of class and try to absorb as much info as I can.",
+  b: "Sit in the back and play Clash Royale.",
+  c: "Sit somewhere in the middle, with my friends.",
+  d: "I do not go to class ;-;",
+  e: "Lowkey I be the professor bro."
+};
+
+export const evilHobbyMap: Record<string, string> = {
+  a: "Gossiping the latest tea",
+  b: "Splurging money on (useless?) items",
+  c: "Mukbanging food",
+  d: "Entering a ranked match in Valorant",
+  e: "Going down Wikipedia rabbit holes"
+};

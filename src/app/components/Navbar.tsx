daaae5bcafc1e9227bc0bc73788df29c57b23253
Monica <monica@assetflo.com>
2025-06-03 16:33:41 -0400

    tweak

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   const dispatch = useDispatch<AppDispatch>();
 
   const handleClick = () => {
-    router.push("/home");
+    router.push("/");
   }
 
   const handleSignOut = () => {
@@ -33,15 +33,15 @@ const Navbar = () => {
       </div>
 
       <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2">
-        <span className="text-lg md:text-2xl font-bold tracking-wide text-[#374995] italic font-jakarta">
-          Speed-<span className='text-[#5989fc] font-jakarta'>Data</span>ing Social
+        <span className="text-md md:text-2xl font-bold tracking-wide text-[#374995] italic font-jakarta">
+          Speed-<span className='text-[#5989fc] font-jakarta'>Friend</span>ing Social
         </span>
       </div>
 
       <div className="hidden md:flex items-center gap-6">
-        <a href="/home" className="text-[#374995] cursor-pointer font-semibold hover:text-blue-500 font-medium transition font-jakarta">Dashboard</a>
-        <a href="/information" className="text-[#374995] cursor-pointer font-semibold hover:text-blue-500 font-medium transition font-jakarta">How it works</a>
-        <button onClick={handleSignOut} className="rounded-xl cursor-pointer font-semibold p-2 hover:bg-[#e1eaf8] transition font-jakarta">Log Out</button>
+        <a href="/dashboard" className="text-[#374995] cursor-pointer font-semibold hover:text-blue-500 font-medium transition font-jakarta">Dashboard</a>
+        <a href="#info" className="text-[#374995] cursor-pointer font-semibold hover:text-blue-500 font-medium transition font-jakarta">How it works</a>
+        <button onClick={handleSignOut} className="rounded-xl cursor-pointer font-semibold px-4 py-2 hover:bg-[#e1eaf8] transition text-white bg-blue-500">Log Out</button>
       </div>
       
 
@@ -69,9 +69,9 @@ const Navbar = () => {
           aria-label="Close menu"
         >
         </button>
-        <a href="/" className="text-2xl text-jakarta text-[#374995] hover:text-blue-500 font-bold transition font-jakarta" onClick={() => setMenuOpen(false)}>Dashboard</a>
-        <a href="/about" className="text-2xl text-[#374995] hover:text-blue-500 font-bold transition font-jakarta" onClick={() => setMenuOpen(false)}>How it works</a>
-        <button onClick={handleSignOut} className="rounded-xl p-2 text-[#374995] hover:bg-[#374995] hover:text-white transition font-jakarta">Log Out</button>
+        <a href="/dashboard" className="text-2xl text-jakarta text-[#374995] hover:text-blue-500 font-bold transition font-jakarta" onClick={() => setMenuOpen(false)}>Dashboard</a>
+        <a href="#infoMobile" className="text-2xl text-[#374995] hover:text-blue-500 font-bold transition font-jakarta" onClick={() => setMenuOpen(false)}>How it works</a>
+        <button onClick={handleSignOut} className="rounded-xl p-2 text-[#374995] hover:bg-[#374995] bg-blue-500 text-white px-8 hover:text-white transition font-jakarta">Log Out</button>
 
       </div>
     </nav>
---
+++ src/app/dashboard/Instruction.tsx
@@ -0,0 +1,157 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import Image from 'next/image'
+import instructionBackground from '../../../public/images/dashboard/InstructionBackground.svg'
+import whale1 from '../../../public/images/dashboard/whaleLightBulb.svg'
+import whale2 from '../../../public/images/dashboard/whaleCup.svg'
+import fancyText from '../../../public/images/dashboard/fancytext1.svg'
+import StepCard from './StepCard'
+
+export default function InstructionPage() {
+  const [showBg, setShowBg] = useState(true)
+
+  useEffect(() => {
+    // Function to check screen size
+    const checkScreen = () => {
+      setShowBg(window.innerWidth < 1024) 
+    }
+    checkScreen()
+    window.addEventListener('resize', checkScreen)
+    return () => window.removeEventListener('resize', checkScreen)
+  }, [])
+
+  return (
+  <section>
+    {/*mobile view*/}
+    {showBg ? (
+    <div className="w-full bg-[#E6EFFD] flex flex-col items-center pb-30 sm:pb-40 md:pb-45"> {/*FIXXX*/}
+      <div id="infoMobile" className="relative w-[90%] max-w-6xl aspect-[2/3] rounded-[50px] mx-auto">
+        <Image
+          src={fancyText}
+          alt="How to participate?"
+          className="h-15 sm:h-19 md:h-24 lg:h-30 w-auto mx-auto mt-2 md:mt-5 mb-4 md:mb-6"
+          priority
+        />
+        <div
+          className="relative w-full h-full rounded-[20px]"
+          style={{
+            ...(showBg
+              ? {
+                  backgroundImage: `url(${instructionBackground.src})`,
+                  backgroundSize: 'cover',
+                  backgroundPosition: 'center',
+                  backgroundRepeat: 'no-repeat',
+                }
+              : {}),
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <div className="relative z-30 w-full h-full flex items-center justify-center">
+            <div className="grid grid-cols-2 gap-6 w-[90%] -mt-6 -mb-6">
+              <StepCard
+                step="Step 1:"
+                description={
+                  <>
+                    <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+                    event or something psum that this is a good event or something psum that this is a good event or something
+                  </>
+                }
+                opacity={0.8}
+                icon={whale2}
+              />
+              <StepCard
+                step="Step 2:"
+                description={
+                  <>
+                    <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+                    event or something psum that this is a good event or something psum that this is a good event or something
+                  </>
+                }
+                opacity={0.4}
+              />
+              <StepCard
+                step="Step 3:"
+                description={
+                  <>
+                    <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+                    event or something psum that this is a good event or something psum that this is a good event or something
+                  </>
+                }
+                opacity={0.4}
+              />
+              <StepCard
+                step="Step 4:"
+                description={
+                  <>
+                    <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+                    event or something psum that this is a good event or something psum that this is a good event or something
+                  </>
+                }
+                opacity={0.8}
+                icon={whale1}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    ) : ( 
+    <div id="info" className="w-full bg-[#E6EFFD] flex flex-col items-center"> {/*desktop view*/}
+      <Image
+        src={fancyText}
+        alt="How to participate?"
+        className="h-15 sm:h-19 md:h-24 lg:h-30 w-auto mx-auto mt-2 md:mt-5 mb-4 md:mb-6"
+        priority
+      />
+      <div className="relative z-30 w-full h-full flex items-center justify-center py-10">
+        <div className="grid grid-cols-2 gap-6 w-[90%] -mt-6 -mb-6">
+          <StepCard
+            step="Step 1:"
+            description={
+              <>
+              <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+                event or something psum that this is a good event or something psum that this is a good event or something
+              </>
+            }
+            opacity={0.8}
+            icon={whale2}
+          />
+          <StepCard
+            step="Step 2:"
+            description={
+              <>
+              <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+              event or something psum that this is a good event or something psum that this is a good event or something
+              </>
+            }
+            opacity={0.4}
+          />            
+          <StepCard
+            step="Step 3:"
+            description={
+              <>
+              <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+              event or something psum that this is a good event or something psum that this is a good event or something
+              </>
+            }
+            opacity={0.4}
+          />
+          <StepCard
+            step="Step 4:"
+            description={
+              <>
+              <span className="font-bold italic">Lorem</span> ipsum that this is a good event or something psum that this is a good
+              event or something psum that this is a good event or something psum that this is a good event or something
+              </>
+            }
+            opacity={0.8}
+            icon={whale1}
+          />
+        </div>
+      </div>
+    </div>
+    )}
+  </section>
+  )
+}
